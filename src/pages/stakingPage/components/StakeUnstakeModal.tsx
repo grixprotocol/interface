fd5d9e32387d787a92ce7e494ccc1333efbf3ff4
asalef10 <asalef10@gmail.com>
2025-04-23 13:03:55 +0300

    lint

--- src/pages/stakingPage/components/StakeUnstakeModal.tsx
+++ src/pages/stakingPage/components/StakeUnstakeModal.tsx
@@ -14,6 +14,7 @@ import {
 import React, { useMemo } from 'react';
 
 import { GrixLogo } from '@/components/commons/Logo';
+
 import { formatBalance } from '../utils/formatters';
 
 type StakeUnstakeModalProps = {
