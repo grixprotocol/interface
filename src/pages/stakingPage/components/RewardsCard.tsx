7eb936d12d07d1fc62abc8c21b0884a7f72b17f2
asalef10 <asalef10@gmail.com>
2025-06-09 17:35:21 +0300

    Use grix api token

--- src/pages/stakingPage/components/RewardsCard.tsx
+++ src/pages/stakingPage/components/RewardsCard.tsx
@@ -36,9 +36,14 @@ export const RewardsCard = ({ data, refetchData }: RewardsCardProps): JSX.Elemen
   useEffect(() => {
     const fetchPrice = async () => {
       try {
-        const res = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=grix&vs_currencies=usd');
-        const json = (await res.json()) as DexScreenerResponse;
-        const price = json.grix.usd;
+        const res = await fetch('https://z61hgkwkn8.execute-api.us-east-1.amazonaws.com/dev/assetprice?asset=GRIX', {
+          headers: {
+            'x-api-key': import.meta.env.VITE_GRIX_API_KEY,
+            origin: 'https://app.grix.finance',
+          },
+        });
+        const json = await res.json();
+        const price = json.assetPrice;
         setGrixPrice(price);
       } catch {
         setGrixPrice(null);
